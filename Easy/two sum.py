398c04e83959cec0cb444da79d7b0de14945722f
yashydv <68542825+yashydv@users.noreply.github.com>
2021-06-24 20:40:18 +0530

    Update two sum.py

--- Easy/two sum.py
+++ Easy/two sum.py
@@ -1,1 +1,9 @@
-
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		idx_1, idx_2 = 0,1
+		first={}
+		for i,  item in enumerate(nums):
+			if (target-item) in first:
+				return [first[target-item], i]
+				break
+			first[item] = i
