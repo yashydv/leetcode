a4f54a2b5bdd26ff12d512388a2b4f5a268d9e1b
yashydv <68542825+yashydv@users.noreply.github.com>
2021-07-05 23:55:38 +0530

    Create #3.js

---
+++ Medium/#3.js
@@ -0,0 +1,18 @@
+var lengthOfLongestSubstring = function(s) {
+  
+    var l = 0;
+    var hashedChars = {};
+    
+    const result = s.split("").reduce((longest, c, idx) => {
+        
+        if(hashedChars[c] !== undefined) {
+            l = Math.max(l, hashedChars[c] + 1);
+        }
+        
+        hashedChars[c] = idx;
+        
+        return Math.max(idx - l + 1, longest);
+    }, 0);
+    
+    return result;
+}
